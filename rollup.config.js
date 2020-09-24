5189551e67fce547792c3acd1b259015162b98a4
Avinash <avinashpsk@gmail.com>
2020-09-24 17:49:49 +0530

    publish

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,23 @@ import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
-
+import { terser } from "rollup-plugin-terser";
+import { uglify } from "rollup-plugin-uglify";
 import packageJSON from "./package.json";
+
 const input = "./src/index.js";
+const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
 
 export default [
-    // CommonJS
+    // CommonJS ...
+    // UMD ...
+    // ES
     {
         input,
         output: {
-            file: packageJSON.main,
-            format: "cjs"
+            file: packageJSON.module,
+            format: "es",
+            exports: "named"
         },
         plugins: [
             babel({
@@ -21,6 +27,23 @@ export default [
             external(),
             resolve(),
             commonjs()
+        ]
+    },
+    {
+        input,
+        output: {
+            file: minifyExtension(packageJSON.module),
+            format: "es",
+            exports: "named"
+        },
+        plugins: [
+            babel({
+                exclude: "node_modules/**"
+            }),
+            external(),
+            resolve(),
+            commonjs(),
+            terser()
         ]
     }
 ];
